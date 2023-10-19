e8aa4746abdee9f45f233939cca9d5016328a572
Its_Shojib <mdshojib922@gmail.com>
2023-10-19 21:52:07 +0600

    Deploy issue solved

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     let productCollection = client.db('ProductDB').collection("Products");
     let cartCollection = client.db('ProductDB').collection('MyCart')
@@ -145,7 +145,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
